88d14e84bd15c40be58166bca19f2d30c8162732
Unknown <axel.raffatin@sfr.fr>
2019-05-24 20:40:45 -0400

    Boat Movement

--- CanoeVR/Assets/Script/Canoe.cs
+++ CanoeVR/Assets/Script/Canoe.cs
@@ -4,8 +4,28 @@ using UnityEngine;
 
 public class Canoe : MonoBehaviour
 {
-    void Update()
+    [SerializeField] private Transform canoe;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float speedTranslation;
+    [SerializeField] private float speedRotation;
+
+    void Start() {
+        speedTranslation = 10.0f;
+        speedRotation = 5.0f;
+    }
+
+    void FixedUpdate()
     {
-        GetComponent<Rigidbody>().AddForce(Vector3.forward * 10, ForceMode.Force);
+        if(Input.GetKeyDown(KeyCode.K)) {
+            rb.AddForce(canoe.forward * speedTranslation, ForceMode.Acceleration);
+            rb.AddForce(-canoe.right * speedRotation, ForceMode.Acceleration);
+            rb.AddTorque(-canoe.up * speedRotation, ForceMode.Acceleration);
+        }
+
+        if(Input.GetKeyDown(KeyCode.M)) {
+            rb.AddForce(canoe.forward * speedTranslation, ForceMode.Acceleration);
+            rb.AddForce(canoe.right * speedRotation, ForceMode.Acceleration);
+            rb.AddTorque(canoe.up * speedRotation, ForceMode.Acceleration);
+        }
     }
 }
