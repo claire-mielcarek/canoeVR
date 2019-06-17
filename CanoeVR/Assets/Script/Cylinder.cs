770afec486b85cf4c01e26253a53a36b6f3a332c
claire-mielcarek <claire.mielcarek@lilo.org>
2019-06-17 18:43:59 -0400

    Add mountains and began to create cylinder for slalom

---
+++ CanoeVR/Assets/Script/Cylinder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cylinder : MonoBehaviour
+{
+
+    [SerializeField] private string side;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(side))
+        {
+            Debug.Log("Good side !");
+            Ring();
+        }
+        else
+        {
+            Debug.Log("Wrong side ! :(");
+        }
+    }
+
+    void Ring()
+    {
+        Debug.Log("Ajouter un son cool ici");
+    }
+}
